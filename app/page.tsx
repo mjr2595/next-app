2dafdfeda7c3f7bd33d85687f5e0716a2cca24da
mjr2595 <michaeljohnraymond@gmail.com>
2024-03-11 23:43:49 -0500

    css modules -> tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
-import ProductCard from "./components/ProductCard/ProductCard";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
   return (
