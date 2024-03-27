7718040a44de039e2a05c1f73b13270007e5fec2
mjr2595 <michaeljohnraymond@gmail.com>
2024-03-26 21:47:18 -0500

    Add styles to WelcomeTemplate with CSSProperties

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -6,20 +6,29 @@ import {
   Preview,
   Text,
 } from "@react-email/components";
-import React from "react";
+import React, { CSSProperties } from "react";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Howdy!</Preview>
-      <Body>
+      <Body style={body}>
         <Container>
-          <Text>Hello {name}</Text>
+          <Text style={heading}>Hello {name}</Text>
           <Link href="https://google.com">Get Started</Link>
         </Container>
       </Body>
     </Html>
   );
+};
+
+const body: CSSProperties = {
+  background: "#fff",
+};
+
+const heading: CSSProperties = {
+  fontSize: "32px",
+  fontWeight: "bold",
 };
 
 export default WelcomeTemplate;
